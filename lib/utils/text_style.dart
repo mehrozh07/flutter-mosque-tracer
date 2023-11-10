8ecb9df944d65567fc71a0a0f36ec3b0f657cbd1
Mehrozh <mehroozhassan@itsol.studio>
2023-11-11 00:30:13 +0500

    field

---
+++ lib/utils/text_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/painting/text_style.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InterStyle{
+
+  static TextStyle w600f16Black = GoogleFonts.inter(
+
+  );
+
+  static TextStyle fieldErrStyle = GoogleFonts.inter(
+    color: Colors.red,
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+
+}
---
+++ lib/widgets/custom_field.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mosque_tracer/utils/colors.dart';
+import 'package:mosque_tracer/utils/text_style.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  final String? hintText, labelText, helperText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon, prefixIconWidget;
+  final FormFieldValidator<dynamic>? validator;
+  final TextEditingController? controller;
+  final Color? enableBorderColor;
+  final bool showLabelText;
+  final int? maxLines,maxLength;
+  final Color? fillColor;
+  // final EdgeInsets? padding;
+  final bool isPadding, filled;
+  final TextInputType? textInputType;
+  final bool showPrefixWidget;
+  final bool readOnly;
+  final Function()? onTap;
+  final Function(String)? onChange;
+  final List<TextInputFormatter>? format;
+  final TextInputAction? textInputAction;
+  const CustomTextFiled({
+    Key? key,
+    this.maxLength = 500,
+    this.onChange,
+    this.textInputAction,
+    this.readOnly=false,
+    this.onTap,
+    this.format,
+    this.labelText,
+    this.prefixIconWidget,
+    this.showPrefixWidget = false,
+    // this.padding,
+    this.textInputType,
+    this.fillColor,
+    this.isPadding = false,
+    this.filled = true,
+    this.maxLines = 1,
+    this.showLabelText = false,
+    this.helperText,
+    this.validator,
+    this.hintText,
+    this.prefixIcon,
+    this.controller,
+    this.suffixIcon,
+    this.enableBorderColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      maxLines: maxLines,
+      maxLength: maxLength,
+      inputFormatters: format,
+      readOnly: readOnly,
+      onTap: onTap,
+      onChanged: onChange,
+      keyboardType: textInputType,
+      textInputAction: textInputAction,
+      style: Theme.of(context).textTheme.labelSmall,
+      onTapOutside: (event)=>FocusManager.instance.primaryFocus?.unfocus(),
+      decoration: InputDecoration(
+        counterText: '',
+        fillColor: filled ? Colors.transparent : fillColor,
+        filled: filled,
+        contentPadding: const EdgeInsets.only(left: 12),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: kPrimaryColor,width: 2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: kPrimaryColor),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: Colors.grey.shade100),
+        ),
+        helperText: helperText,
+        prefixIcon: showPrefixWidget == false ? prefixIcon : prefixIconWidget,
+        suffixIcon: suffixIcon,
+        prefixIconColor: Colors.black,
+        hintText: "$hintText",
+        errorStyle: InterStyle.fieldErrStyle,
+        labelText: showLabelText ? null : "$labelText",
+        labelStyle: GoogleFonts.inter(
+          fontWeight: FontWeight.w500,
+          color: Colors.grey.shade100,
+          fontSize: 14,
+        ),
+        hintStyle: GoogleFonts.inter(
+          fontWeight: FontWeight.w400,
+          color: Colors.grey.shade100,
+          fontSize: MediaQuery.textScaleFactorOf(context)*13,
+        ),
+      ),
+    );
+  }
+}
