63efbd1c16e2126bb9039ea81b2e35d1a3384d24
Mehrozh <mehroozhassan@itsol.studio>
2023-11-11 16:07:15 +0500

    map

--- lib/View/dashboard/Home-Screen/nearby_mosque.dart
+++ lib/View/dashboard/Home-Screen/nearby_mosque.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mosque_tracer/utils/text_style.dart';
 
 class NearByMosque extends StatefulWidget {
   const NearByMosque({super.key});
@@ -35,71 +35,91 @@ class _NearByMosqueState extends State<NearByMosque> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: Stack(
-        children: [
-
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              height: size.height * 0.8,
-              width: double.infinity,
-              alignment: Alignment.topCenter,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade50,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(32),
-                  topRight: Radius.circular(32),
-                ),
-              ),
-              child: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Stack(
+          children: [
+            Positioned(
+              top: size.height*0.04,
+              right: 0,
+              left: 0,
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    height: size.height*0.3,
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.symmetric(horizontal: size.width*0.04),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40)
-                    ),
-                    child: Stack(
-                      alignment: Alignment.topRight,
-                      children: [
-                      GoogleMap(
-                      mapType: MapType.normal,
-                      compassEnabled: true,
-                      myLocationButtonEnabled: true,
-                      myLocationEnabled: true,
-                      initialCameraPosition: _kGooglePlex,
-                      onMapCreated: (GoogleMapController controller) {
-                        _controller.complete(controller);
-                        _determinePosition();
-                      },
-                    ),
-                        // Transform.scale(
-                        //   scale: 0.7,
-                        //   child: IconButton(
-                        //     style: IconButton.styleFrom(
-                        //       backgroundColor: Theme.of(context).primaryColor,
-                        //       shape: const CircleBorder(),
-                        //     ),
-                        //       onPressed: _determinePosition,
-                        //       icon: const Icon(Icons.location_searching,color: Colors.white),
-                        //   ),
-                        // )
-                      ],
-                    ),
-                  )
+                  const SizedBox.shrink(),
+                  Text('Mosque near you',style: InterStyle.w600f16Black),
+                   Padding(
+                    padding: EdgeInsets.only(right: size.width*0.032),
+                    child: const Icon(Icons.arrow_forward_outlined),
+                  ),
                 ],
               ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                height: size.height * 0.75,
+                width: double.infinity,
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.symmetric(horizontal: size.width*0.04),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade50,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Map',style: InterStyle.w600f16Black),
+                    const SizedBox(height: 12),
+                    Container(
+                      width: double.infinity,
+                      height: size.height*0.3,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                        GoogleMap(
+                        mapType: MapType.normal,
+                        compassEnabled: true,
+                        myLocationButtonEnabled: true,
+                        myLocationEnabled: true,
+                        initialCameraPosition: _kGooglePlex,
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                          _determinePosition();
+                        },
+                      ),
+                          // Transform.scale(
+                          //   scale: 0.7,
+                          //   child: IconButton(
+                          //     style: IconButton.styleFrom(
+                          //       backgroundColor: Theme.of(context).primaryColor,
+                          //       shape: const CircleBorder(),
+                          //     ),
+                          //       onPressed: _determinePosition,
+                          //       icon: const Icon(Icons.location_searching,color: Colors.white),
+                          //   ),
+                          // )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
