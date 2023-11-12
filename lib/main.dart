1ddcfd9dd344c5b9ceadb9313dee644c3f35adaa
Mehrozh <mehroozhassan@itsol.studio>
2023-11-12 22:52:06 +0500

    prayer-timing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mosque_tracer/View/splash-view/splash_view.dart';
 import 'package:mosque_tracer/firebase_options.dart';
 import 'package:mosque_tracer/model-view/auth-notifier.dart';
@@ -10,6 +13,7 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = MyBlockObserver();
   runApp(MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => AuthNotifier())],
       child: const MyApp()));
@@ -29,5 +33,39 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
     );
+  }
+}
+
+
+class MyBlockObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    log("Bloc create at $bloc");
+    super.onCreate(bloc);
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    log("Bloc close at $bloc");
+    super.onClose(bloc);
+  }
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    log("Bloc change at $bloc: $change");
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log("Bloc error at $bloc: Error:$error: stack trace:$stackTrace");
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onTransition(
+      Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
+    log("Bloc transition at $bloc: $transition");
+    super.onTransition(bloc, transition);
   }
 }
